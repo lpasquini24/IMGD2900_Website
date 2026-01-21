506b25b2cb5af05ab99b45d80f3b10c2622c69b1
lpasquini24 <123428916+lpasquini24@users.noreply.github.com>
2026-01-21 14:51:27 -0500

    Initial commit

---
+++ Website/resources/Dev1/game.js
@@ -0,0 +1,352 @@
+/*
+game.js for Perlenspiel 3.3.x
+Last revision: 2022-03-15 (BM)
+
+Perlenspiel is a scheme by Professor Moriarty (bmoriarty@wpi.edu).
+This version of Perlenspiel (3.3.x) is hosted at <https://ps3.perlenspiel.net>
+Perlenspiel is Copyright © 2009-22 Brian Moriarty.
+This file is part of the standard Perlenspiel 3.3.x devkit distribution.
+
+Perlenspiel is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Perlenspiel is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You may have received a copy of the GNU Lesser General Public License
+along with the Perlenspiel devkit. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+This JavaScript file is a template for creating new Perlenspiel 3.3.x games.
+Any unused event-handling function templates can be safely deleted.
+Refer to the tutorials and documentation at <https://ps3.perlenspiel.net> for details.
+*/
+
+/*
+The following comment lines are for JSHint <https://jshint.com>, a tool for monitoring code quality.
+You may find them useful if your development environment is configured to support JSHint.
+If you don't use JSHint (or are using it with a configuration file), you can safely delete these two lines.
+*/
+
+/* jshint browser : true, devel : true, esversion : 6, freeze : true */
+/* globals PS : true */
+
+"use strict"; // Do NOT remove this directive!
+
+/*
+Data structure to store the positions of all falling blocks in the world.
+ */
+
+// stores a list of all of the physics blocks
+var blocks = [];
+
+var board_size = 12;
+
+// used for handling physics block collisions
+var occupied = //new Array(board_size).fill(new Array(board_size.fill(0)));
+Array.from({ length: board_size}, () => new Array(board_size).fill(0));
+
+var dir_offset = 0;
+
+
+/*
+PS.init( system, options )
+Called once after engine is initialized but before event-polling begins.
+This function doesn't have to do anything, although initializing the grid dimensions with PS.gridSize() is recommended.
+If PS.grid() is not called, the default grid dimensions (8 x 8 beads) are applied.
+Any value returned is ignored.
+[system : Object] = A JavaScript object containing engine and host platform information properties; see API documentation for details.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.init = function( system, options ) {
+	// Uncomment the following code line
+	// to verify operation:
+
+	PS.debug( "PS.init() called\n" );
+
+	// This function should normally begin
+	// with a call to PS.gridSize( x, y )
+	// where x and y are the desired initial
+	// dimensions of the grid.
+	// Call PS.gridSize() FIRST to avoid problems!
+	// The sample call below sets the grid to the
+	// default dimensions (8 x 8).
+	// Uncomment the following code line and change
+	// the x and y parameters as needed.
+
+	PS.gridSize( board_size, board_size );
+
+	// This is also a good place to display
+	// your game title or a welcome message
+	// in the status line above the grid.
+	// Uncomment the following code line and
+	// change the string parameter as needed.
+
+	PS.statusText( "Gravity Blocks" );
+
+	PS.timerStart (5, PS.hello);
+
+	// Add any other initialization code you need here.
+};
+
+PS.hello = function()
+{
+	// update all blocks
+	for(var block of blocks){
+		// move block if it's not locked
+		if(!block.locked) {
+			// record old block coords
+			var oldx = block.x;
+			var oldy = block.y;
+
+			// decide a direction to move in
+			var movevec = PS.dirToVector(block.dir);
+
+			// calculate new block coors
+			var newx = oldx + movevec.x;
+			var newy = oldy + movevec.y;
+
+            // move only if the new position is unoccupied
+			if (PS.coordInBounds(newx, newy) && occupied[newx][newy] == 0) {
+				PS.moveBlock(block, oldx, oldy, newx, newy);
+			}
+		}
+	}
+};
+
+PS.coordInBounds = function(x, y){
+	return (x > -1 && x < board_size && y > -1 && y < board_size);
+}
+
+PS.moveBlock = function(block, oldx, oldy, newx, newy){
+	// bounds checking
+	if(!PS.coordInBounds(newx, newy)) return;
+
+	// update occupied array
+	occupied[oldx][oldy] = 0;
+	occupied[newx][newy] = 1;
+
+	// update block
+	block.x = newx;
+	block.y = newy;
+
+	// hide old block
+	PS.color(oldx, oldy, 255, 255, 255);
+	// draw new block
+	PS.color(newx, newy, PS.dirToColor(block.dir));
+};
+
+PS.removeBlock = function(block){
+	var index = blocks.indexOf(block);
+	if (index == -1) return;
+
+	// update occupied array
+	occupied[block.x][block.y] = 0;
+	// remove block from blocks array
+	blocks.splice(index, 1);
+	// remove block from screen
+	PS.color(x, y, 255, 255, 255);
+}
+
+/*
+Gravity directions are:
+0 - no gravity
+1 - up
+2 - right
+3 - down
+4 - left
+ */
+PS.dirToVector = function(dir){
+	switch(1 + ((dir + dir_offset) % 4)){
+		case 0:
+			return {x: 0, y: 0};
+		case 1:
+			return {x: 0, y: -1};
+		case 2:
+			return {x: 1, y: 0};
+		case 3:
+			return {x: 0, y: 1};
+		case 4:
+			return {x: -1, y: 0};
+	}
+}
+
+PS.dirToColor = function(dir){
+	switch(dir){
+		case 0:
+			return PS.COLOR_BLACK;
+		case 1:
+			return PS.COLOR_RED;
+		case 2:
+			return PS.COLOR_BLUE;
+		case 3:
+			return PS.COLOR_GREEN;
+		case 4:
+			return PS.COLOR_YELLOW;
+	}
+}
+
+PS.placeBlock = function(x, y, locked, dir){
+	if(!PS.coordInBounds(x, y)) return;
+	// update occupied array
+	occupied[x][y] = 1;
+	// add block to blocks array
+	blocks.push({x: x, y: y, locked: locked, dir: dir});
+	// render block
+	PS.color(x, y, PS.dirToColor(dir));
+};
+
+/*
+PS.touch ( x, y, data, options )
+Called when the left mouse button is clicked over bead(x, y), or when bead(x, y) is touched.
+This function doesn't have to do anything. Any value returned is ignored.
+[x : Number] = zero-based x-position (column) of the bead on the grid.
+[y : Number] = zero-based y-position (row) of the bead on the grid.
+[data : *] = The JavaScript value previously associated with bead(x, y) using PS.data(); default = 0.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.touch = function( x, y, data, options ) {
+	// Uncomment the following code line
+	// to inspect x/y parameters:
+
+	// PS.debug( "PS.touch() @ " + x + ", " + y + "\n" );
+
+	// Add code here for mouse clicks/touches
+	// over a bead.
+	PS.placeBlock(x, y, false, PS.random(4));
+};
+
+/*
+PS.release ( x, y, data, options )
+Called when the left mouse button is released, or when a touch is lifted, over bead(x, y).
+This function doesn't have to do anything. Any value returned is ignored.
+[x : Number] = zero-based x-position (column) of the bead on the grid.
+[y : Number] = zero-based y-position (row) of the bead on the grid.
+[data : *] = The JavaScript value previously associated with bead(x, y) using PS.data(); default = 0.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.release = function( x, y, data, options ) {
+	// Uncomment the following code line to inspect x/y parameters:
+
+	// PS.debug( "PS.release() @ " + x + ", " + y + "\n" );
+
+	// Add code here for when the mouse button/touch is released over a bead.
+};
+
+/*
+PS.enter ( x, y, button, data, options )
+Called when the mouse cursor/touch enters bead(x, y).
+This function doesn't have to do anything. Any value returned is ignored.
+[x : Number] = zero-based x-position (column) of the bead on the grid.
+[y : Number] = zero-based y-position (row) of the bead on the grid.
+[data : *] = The JavaScript value previously associated with bead(x, y) using PS.data(); default = 0.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.enter = function( x, y, data, options ) {
+	// Uncomment the following code line to inspect x/y parameters:
+
+	// PS.debug( "PS.enter() @ " + x + ", " + y + "\n" );
+
+	// Add code here for when the mouse cursor/touch enters a bead.
+};
+
+/*
+PS.exit ( x, y, data, options )
+Called when the mouse cursor/touch exits bead(x, y).
+This function doesn't have to do anything. Any value returned is ignored.
+[x : Number] = zero-based x-position (column) of the bead on the grid.
+[y : Number] = zero-based y-position (row) of the bead on the grid.
+[data : *] = The JavaScript value previously associated with bead(x, y) using PS.data(); default = 0.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.exit = function( x, y, data, options ) {
+	// Uncomment the following code line to inspect x/y parameters:
+
+	// PS.debug( "PS.exit() @ " + x + ", " + y + "\n" );
+
+	// Add code here for when the mouse cursor/touch exits a bead.
+};
+
+/*
+PS.exitGrid ( options )
+Called when the mouse cursor/touch exits the grid perimeter.
+This function doesn't have to do anything. Any value returned is ignored.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.exitGrid = function( options ) {
+	// Uncomment the following code line to verify operation:
+
+	// PS.debug( "PS.exitGrid() called\n" );
+
+	// Add code here for when the mouse cursor/touch moves off the grid.
+};
+
+/*
+PS.keyDown ( key, shift, ctrl, options )
+Called when a key on the keyboard is pressed.
+This function doesn't have to do anything. Any value returned is ignored.
+[key : Number] = ASCII code of the released key, or one of the PS.KEY_* constants documented in the API.
+[shift : Boolean] = true if shift key is held down, else false.
+[ctrl : Boolean] = true if control key is held down, else false.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.keyDown = function( key, shift, ctrl, options ) {
+	// Uncomment the following code line to inspect first three parameters:
+
+	// PS.debug( "PS.keyDown(): key=" + key + ", shift=" + shift + ", ctrl=" + ctrl + "\n" );
+
+	// Add code here for when a key is pressed.
+	dir_offset += 1;
+};
+
+/*
+PS.keyUp ( key, shift, ctrl, options )
+Called when a key on the keyboard is released.
+This function doesn't have to do anything. Any value returned is ignored.
+[key : Number] = ASCII code of the released key, or one of the PS.KEY_* constants documented in the API.
+[shift : Boolean] = true if shift key is held down, else false.
+[ctrl : Boolean] = true if control key is held down, else false.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+*/
+
+PS.keyUp = function( key, shift, ctrl, options ) {
+	// Uncomment the following code line to inspect first three parameters:
+
+	// PS.debug( "PS.keyUp(): key=" + key + ", shift=" + shift + ", ctrl=" + ctrl + "\n" );
+
+	// Add code here for when a key is released.
+};
+
+/*
+PS.input ( sensors, options )
+Called when a supported input device event (other than those above) is detected.
+This function doesn't have to do anything. Any value returned is ignored.
+[sensors : Object] = A JavaScript object with properties indicating sensor status; see API documentation for details.
+[options : Object] = A JavaScript object with optional data properties; see API documentation for details.
+NOTE: Currently, only mouse wheel events are reported, and only when the mouse cursor is positioned directly over the grid.
+*/
+
+PS.input = function( sensors, options ) {
+	// Uncomment the following code lines to inspect first parameter:
+
+//	 var device = sensors.wheel; // check for scroll wheel
+//
+//	 if ( device ) {
+//	   PS.debug( "PS.input(): " + device + "\n" );
+//	 }
+
+	// Add code here for when an input event is detected.
+};
+
